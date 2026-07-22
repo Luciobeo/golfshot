8a2744517c3ba2e2a7301cb6b1de8da4fe4652ad
Luciobeo <andrealuciani76@gmail.com>
2026-07-22 11:14:44 +0200

    Add files via upload

---
+++ andrea-golf-caddie/andrea-golf-caddie/sw.js
@@ -0,0 +1,28 @@
+const CACHE='andrea-golf-caddie-v8-user-art';
+const ASSETS=[
+  './','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png',
+  './assets/campi-clean.jpg','./assets/sacca-clean.jpg','./assets/partite-clean.jpg',
+  './assets/bg-home.jpg','./assets/bg-play.jpg','./assets/bg-finish.jpg','./assets/bg-app.jpg','./assets/bg-panel.jpg','./assets/logo-andrea.png'
+];
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),
+    self.clients.claim()
+  ]));
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{
+      const copy=response.clone(); caches.open(CACHE).then(cache=>cache.put('./index.html',copy)); return response;
+    }).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+    const copy=response.clone(); caches.open(CACHE).then(cache=>cache.put(event.request,copy)); return response;
+  })));
+});
