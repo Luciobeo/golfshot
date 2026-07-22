dc1168bb51d1f555f3170ec1174cc5c22491064e
Luciobeo <andrealuciani76@gmail.com>
2026-07-22 21:43:37 +0200

    Delete andrea-golf-caddie/andrea-golf-caddie directory

--- andrea-golf-caddie/andrea-golf-caddie/sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE='andrea-golf-caddie-v12-newcard';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
-self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))),self.clients.claim()]));});
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  if(e.request.mode==='navigate'){e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',cp));return r;}).catch(()=>caches.match('./index.html')));return;}
-  e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(ca=>ca.put(e.request,cp));return r;})));
-});
